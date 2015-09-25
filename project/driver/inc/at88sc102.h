9dc57cdab8609b0bd5f4100911f174fd97268e7d
rogerlee0824 <roger_lee0824@126.com>
2015-09-24 23:04:09 -0700

    2015-09-24，Commited.

--- project/driver/inc/at88sc102.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AT88SC102_H_
-#define _AT88SC102_H_
-
-#include "stm8l15x.h"
-
-void AT88SC102_Init(void);
-
-#endif
--- project/usr/inc/app_trace.h
+++
@@ -1,9 +0,0 @@
-#ifndef _APP_TRACE_H_
-#define _APP_TRACE_H_
-
-#include "stm8l15x.h"
-
-void app_trace_init(void);
-void UartSendMultiBytes(uint8_t * pbuf,uint16_t len);
-
-#endif
